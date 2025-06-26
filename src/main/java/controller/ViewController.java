f9356bd7892a2e726d69e2105abe235117c7d3a1
Zaloriel <pavel.vitk@mail.ru>
2025-06-26 12:00:42 +0300

    Интерфейс

---
+++ src/main/java/controller/ViewController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String redirectToUsers() {
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String showUserInterface() {
+        return "users";
+    }
+}
